6524f78788fccbbe6af06b9aa890c3c98fe6eb40
angoroa <147700079+angoroa@users.noreply.github.com>
2026-01-07 18:18:08 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 124 ms, Memory: 14724 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int space = 0;
+        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+        speakProfessor(N, space);
+    }
+    static void speakProfessor(int count, int s){
+        if (count == 0){
+            System.out.println("____".repeat(s) + "\"재귀함수가 뭔가요?\"");
+            System.out.println("____".repeat(s) + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+            System.out.println("____".repeat(s) + "라고 답변하였지.");
+            return;
+
+        } 
+        else{
+            System.out.println("____".repeat(s) + "\"재귀함수가 뭔가요?\"");
+            System.out.println("____".repeat(s) + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+            System.out.println("____".repeat(s) + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+            System.out.println("____".repeat(s) + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+            speakProfessor(count-1, s+1);
+            System.out.println("____".repeat(s) + "라고 답변하였지.");                               
+        }
+    }
+}
