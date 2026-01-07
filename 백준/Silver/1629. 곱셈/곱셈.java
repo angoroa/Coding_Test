7bb0b0ca08a7eb3e1a6c2af04e8b65142c880480
angoroa <147700079+angoroa@users.noreply.github.com>
2026-01-07 16:22:50 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,33 +1,45 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    static long A, B, C;
-
-    public static void main(String[] args) throws Exception {
+public class Main{
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        A = Long.parseLong(st.nextToken());
-        B = Long.parseLong(st.nextToken());
-        C = Long.parseLong(st.nextToken());
-
-        System.out.println(pow(A, B));
+        // 10 ^ 11 % 12
+        // 1 * 1 * 1 * 1 * 1 % 2 = 1
+        // 3 * 3 * 3 * 3 * 3 % 2 = 1
+        
+        // A * A * A * A * A % C
+        // (A%C) * (A%C) * (A%C) * (A%C) % (A%C) % C
+        // (A%C)를 B번
+        
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+        long answer = answerfunc(A,B,C);
+        System.out.println(answer);
     }
-
-    static long pow(long a, long b) {
-        // 기저 조건
-        if (b == 0) return 1;
-
-        // 재귀로 절반 계산
-        long half = pow(a, b / 2);
-
-        // 짝수 / 홀수 분기
-        if (b % 2 == 0) {
-            return (half * half) % C;
-        } else {
-            return (half * half % C) * (a % C) % C;
+    static long answerfunc(long A, long B, long C){
+        if(B == 1){
+            return (A % C);
         }
+        if(B == 0){
+            return (1%C);
+        }
+        
+        long half = answerfunc(A,B/2,C);
+        
+        if (B%2==0){
+            //짝수
+            return (half * half) %C % C;
+        }
+        else{
+            //홀수
+            return (half * half) % C * A % C;
+        }           
+
+
+            
     }
+    
 }
