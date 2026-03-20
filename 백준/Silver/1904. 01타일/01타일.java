1f5d6b337d0eed753d8bf3fbcfb7796bb713e39d
angoroa <147700079+angoroa@users.noreply.github.com>
2026-03-20 20:52:17 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 18596 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int dp[] = new int[N+1];
+        dp[0] = 1; dp[1] = 1;
+        for(int i=2; i<=N; i++){
+            dp[i] = (dp[i-2]+dp[i-1])%15746;
+        }
+        System.out.print(dp[N]);
+    }
+}
