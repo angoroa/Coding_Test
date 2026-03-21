bd851b5c6d11627478f41007a3f965c8139106ef
angoroa <147700079+angoroa@users.noreply.github.com>
2026-03-21 17:02:31 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int count =0;
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        for(int i = N-1; i>=0; i--){
+            if(K/arr[i]>0){
+              count += K/arr[i];
+              K -= K/arr[i] * arr[i];
+            } 
+        }
+        System.out.println(count);
+    }
+}
