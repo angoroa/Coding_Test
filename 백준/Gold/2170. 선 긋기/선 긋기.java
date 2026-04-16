373ff2bd78425f7924d80ff6c51697a458d2797a
angoroa <147700079+angoroa@users.noreply.github.com>
2026-04-16 16:59:54 +0900

    [Gold V] Title: 선 긋기, Time: 2252 ms, Memory: 286072 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -17,9 +17,9 @@ public class Main{
                arr[i][1] = Integer.parseInt(st.nextToken());
            }
                Arrays.sort(arr, (a,b)->{
-           if(a[0] == b[0]){
+           if(a[0] == b[0])
                return a[1] - b[1];
-           }
+
            return a[0] - b[0];
        });
        int start = arr[0][0];
