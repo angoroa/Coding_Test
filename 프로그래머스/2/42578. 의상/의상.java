d417e48462cd08504e0d804582372eaef2400d41
angoroa <147700079+angoroa@users.noreply.github.com>
2026-05-17 18:53:53 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,21 +2,15 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<clothes.length; i++){
+                map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
+            }
+                int answer = 1;
         
-        for(int i=0; i< clothes.length; i++){
-            String type = clothes[i][1];
-            
-            if(map.containsKey(type)){
-                map.put(type, map.get(type)+1);
-            }
-            else{
-                map.put(type, 1);
-            }
+        for(int v: map.values()){
+            answer *= (v+1);
         }
-        int answer = 1;
-        for(int count : map.values()){
-            answer*=(count+1);
+        return answer -1;
         }
-        return answer-1;
+
     }
-}
