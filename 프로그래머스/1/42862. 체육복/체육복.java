93debaba161086b6ac92971009c609ddb6d78e72
angoroa <147700079+angoroa@users.noreply.github.com>
2026-08-06 11:32:42 +0900

    [level 1] Title: 체육복, Time: 3.01 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,31 +3,36 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         // [2,4] [1, 3, 5]
-        // 1
-        // 
+        // 1. 정렬
+        // 2. 여벌인데 도난당한 학생 처리
+        // 3. for문으로 빌려주기
+        // 4. return
         Arrays.sort(lost);
         Arrays.sort(reserve);
-        int count =0;
 
         for (int i=0; i<lost.length; i++){
             for (int j=0; j<reserve.length; j++){
                 if (lost[i] == reserve[j]){
                     lost[i] = reserve[j] = -1;
+                    break;
                 }
             }
         }
-        for(int i=0; i<lost.length; i++){
+        for (int i=0; i<lost.length; i++){
+            if(lost[i] == -1) continue;
             for(int j=0; j<reserve.length; j++){
-                if(lost[i] > 0 && reserve[j] > 0 && (lost[i] == reserve[j]-1 || lost[i] == reserve[j]+1)){ lost[i] = -1;
-                  reserve[j] = -1;
-                   break;
-                                                                                                          }                  
-                  
+                if (reserve[j] == -1) continue;
+                if (reserve[j] == lost[i]-1 || reserve[j] == lost[i]+1){
+                    reserve[j] = lost[i] = -1;
+                    break;
+                }
             }
         }
+        int count = 0;
         for(int i=0; i<lost.length; i++){
-            if(lost[i] > 0) count++;
+            if(lost[i]!=-1) count++;
         }
         return n - count;
+
     }
 }
